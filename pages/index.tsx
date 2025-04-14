5b168a9adf84a7f2b00624c7fab1b72566033c48
81jongseongj <34089408+81jongseongj@users.noreply.github.com>
2025-04-14 23:00:33 +0900

    Delete pages directory

--- pages/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-blue-50">
-      <h1 className="text-4xl font-bold">Welcome to JJ Swim Lab</h1>
-    </div>
-  );
-}
