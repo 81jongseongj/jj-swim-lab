2dec4cd2128057df6873a3b1cdccce6a6ce72c50
81jongseongj <34089408+81jongseongj@users.noreply.github.com>
2025-04-14 23:02:16 +0900

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-50">
+      <h1 className="text-4xl font-bold">Welcome to JJ Swim Lab</h1>
+    </div>
+  );
+}
